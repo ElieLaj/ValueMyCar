a4f7cc3b36659be16155c4bfed5f5fe25229d7d4
Elie Lajoinie <elie.lajoinie@supdevinci-edu.fr>
2025-01-27 13:57:24 +0100

    Rajout de Cars

--- src/models/brand.model.ts
+++ src/models/brand.model.ts
@@ -1,17 +1,19 @@
-import { count } from 'console';
 import mongoose, { Schema, Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
+import { ICar } from './car.model';
 
 export interface IBrand extends Document {
   id: string;
   name: string;
   country: string;
+  cars: ICar["_id"][];
 }
 
 const BrandSchema: Schema = new Schema({
     id: { type: String, default: uuidv4, unique: true },
     name: { type: String, required: [true, 'Name is required'], unique: true },
     country: { type: String, required: [true, 'Country is required'] },
+    cars: [{ type: Schema.Types.ObjectId, ref: "Car" }],
 },
     { versionKey: false, timestamps: true }
 );
