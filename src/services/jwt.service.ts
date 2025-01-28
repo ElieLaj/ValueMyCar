801a85a039308e68c44df4ac4df409ce9c9ba342
Elie Lajoinie <elie.lajoinie@supdevinci-edu.fr>
2025-01-28 11:21:12 +0100

    Ajout des tokens JWT

---
+++ src/services/jwt.service.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { IUser } from '../models/user.model';
+import { UserPresenter } from '../types/userDtos';
+
+dotenv.config();
+
+export class JWTService {
+  private SECRET_KEY = process.env.SECRET_KEY!;
+  private SECRET_KEY_REFRESH = process.env.SECRET_KEY_REFRESH!;
+
+  generateAccessToken = (user: UserPresenter): string  => {
+    return jwt.sign({ user: user }, this.SECRET_KEY, { expiresIn: "1h" })
+  }
+
+  generateRefreshToken = (user: UserPresenter): string => {
+    return jwt.sign({ user: user }, this.SECRET_KEY_REFRESH, { expiresIn: "7d" })
+  }
+
+  verifyJWT = async (token: string): Promise<any> => {
+    return jwt.verify(token, this.SECRET_KEY);
+  };
+
+  verifyJWTSecret = async (token: string): Promise<any> => {
+    return jwt.verify(token, this.SECRET_KEY_REFRESH);
+  };
+
+  decodeJWT = async (token: string): Promise<any> => {
+    return jwt.decode(token);
+  };
+}
+
