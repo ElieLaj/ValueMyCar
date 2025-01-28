bc3aa86fa616477a47b4300d00f3640940b2fb8b
Elie Lajoinie <elie.lajoinie@supdevinci-edu.fr>
2025-01-28 11:24:41 +0100

    Sécurisation des routes

--- src/routes/brand.route.ts
+++ src/routes/brand.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express"
 import { BrandController } from "../controllers/brand.controller"
+import * as Auth from "../middlewares/auth.middleware"
 
 const router = Router()
 const brandController = new BrandController()
 
-router.post("/", (req, res, next) => brandController.createBrand(req, res, next))
-router.get("/", (req, res, next) => brandController.getBrands(req, res, next))
-router.get("/:id/cars", (req, res, next) => brandController.getBrandCars(req, res, next))
-router.put("/:id", (req, res, next) => brandController.updateBrand(req, res, next))
-router.delete("/:id", (req, res, next) => brandController.deleteBrand(req, res, next))
-router.patch("/:id", (req, res, next) => brandController.patchBrand(req, res, next))
+router.post("/", Auth.checkJWT, (req, res, next) => brandController.createBrand(req, res, next))
+router.get("/", Auth.checkJWT, (req, res, next) => brandController.getBrands(req, res, next))
+router.get("/:id/cars", Auth.checkJWT, (req, res, next) => brandController.getBrandCars(req, res, next))
+router.put("/:id", Auth.checkJWT, (req, res, next) => brandController.updateBrand(req, res, next))
+router.delete("/:id", Auth.checkJWT, (req, res, next) => brandController.deleteBrand(req, res, next))
+router.patch("/:id", Auth.checkJWT, (req, res, next) => brandController.patchBrand(req, res, next))
 
 export const brandRoutes = router
 
--- src/routes/car.route.ts
+++ src/routes/car.route.ts
@@ -1,15 +1,15 @@
 import { Router } from "express"
 import { CarController } from "../controllers/car.controller"
-
+import * as Auth from "../middlewares/auth.middleware"
 const router = Router()
 const carController = new CarController()
 
-router.post("/", (req, res, next) => carController.createCar(req, res, next))
-router.get("/:id", (req, res, next) => carController.getCar(req, res, next))
-router.get("/", (req, res, next) => carController.getCars(req, res, next))
-router.put("/:id", (req, res, next) => carController.updateCar(req, res, next))
-router.delete("/:id", (req, res, next) => carController.deleteCar(req, res, next))
-router.patch("/:id", (req, res, next) => carController.patchCar(req, res, next))
+router.post("/", Auth.checkJWT, (req, res, next) => carController.createCar(req, res, next))
+router.get("/:id", Auth.checkJWT, (req, res, next) => carController.getCar(req, res, next))
+router.get("/", Auth.checkJWT, (req, res, next) => carController.getCars(req, res, next))
+router.put("/:id", Auth.checkJWT, (req, res, next) => carController.updateCar(req, res, next))
+router.delete("/:id", Auth.checkJWT, (req, res, next) => carController.deleteCar(req, res, next))
+router.patch("/:id", Auth.checkJWT, (req, res, next) => carController.patchCar(req, res, next))
 
 export const carRoutes = router
 
