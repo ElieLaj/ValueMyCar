7444bb303efab25f25e31a30bad48b241cfad418
Elie Lajoinie <elie.lajoinie@supdevinci-edu.fr>
2025-01-28 15:04:37 +0100

    Ajout des documentations

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,46 @@ import cors from "cors";
 import { brandRoutes } from "./routes/brand.route";
 import { errorHandler } from "./middlewares/errorHandler";
 import { userRoutes } from "./routes/user.route";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config()
 
 const app = express()
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentation de l\'API du test de Value my Car',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['**/*.yaml'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json())
 app.use(cors());
