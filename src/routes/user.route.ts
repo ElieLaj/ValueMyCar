3b8d0218f7135ebbaf19aeb75fd5a5ff712cfa09
Elie Lajoinie <elie.lajoinie@supdevinci-edu.fr>
2025-02-05 14:05:02 +0100

    Sécurisation de la route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -12,7 +12,7 @@ router.post("/register", Auth.checkRole, (req, res, next) => userController.regi
 router.post("/login",(req, res, next) => userController.login(req, res, next))
 router.post("/refresh-token", Auth.checkJWTSecret, (req, res, next) => userController.refreshToken(req as EncodedRequest, res, next))
 
-router.get("/", Auth.checkJWT, (req, res, next) => userController.getUsers(req, res, next))
+router.get("/", Auth.checkJWT, (req, res, next) => userController.getUsers(req as EncodedRequest, res, next))
 router.get("/:id", Auth.checkJWT, (req, res, next) => userController.getUser(req, res, next))
 
 router.put("/:id", Auth.checkJWT, (req, res, next) => adminMiddleware(req as EncodedRequest, res, next), (req, res, next) => userController.updateUser(req as EncodedRequest, res, next))
